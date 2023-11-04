2873ab0d23ccc988354fa3f1d20c4c5c41831cfd
Luxin Zhang <67583508+LuxinZhang902@users.noreply.github.com>
2023-11-04 18:29:18 -0400

    add all

--- extension/src.js
+++ extension/src.js
@@ -1,41 +1,22 @@
-// var selectedText = "";
-
-// document.addEventListener("mouseup", function () {
-//     selectedText = getSelectedText();
-//     if (selectedText) {
-//         document.getElementById("selectedText").textContent = "Selected Text: " + selectedText;
-//     }
-// });
-
-// // This button will trigger the logging of the selected text
-// document.getElementById("logTextBtn").addEventListener("click", function() {
-//     console.log("Selected Text from button click: " + selectedText);
-// });
-
-
-// function getSelectedText() {
-//     var text = "";
-//     if (window.getSelection) {
-//         text = window.getSelection().toString();
-//     } else if (document.selection && document.selection.type != "Control") {
-//         text = document.selection.createRange().text;
-//     }
-//     return text;
-// }
-
-
-document.getElementById('print-text').addEventListener('click', () => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.scripting.executeScript({
-        target: {tabId: tabs[0].id},
-        function: printSelectedText
-      });
-    });
-  });
+// document.getElementById('print-text').addEventListener('click', () => {
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       chrome.scripting.executeScript({
+//         target: {tabId: tabs[0].id},
+//         function: printSelectedText
+//       });
+//     });
+//   });
   
-  function printSelectedText() {
-    const selection = window.getSelection().toString();
-    if (selection) {
-      console.log('Selected text:', selection);
+//   function printSelectedText() {
+//     const selection = window.getSelection().toString();
+//     if (selection) {
+//       console.log('Selected text:', selection);
+//     }
+//   }
+  document.addEventListener('mouseup', function () {
+    var selectedText = window.getSelection().toString();
+    if (selectedText !== '') {
+        // Send the selected text to the destination page
+        window.localStorage.setItem('selectedText', selectedText);
     }
-  }
+});
