2c1d69554f6c7005259491d94aa0c798e4a974d7
Luxin Zhang <67583508+LuxinZhang902@users.noreply.github.com>
2023-11-04 19:37:32 -0400

    add openai feature

--- extension/popup.js
+++ extension/popup.js
@@ -9,6 +9,8 @@ extensionIcon.style.cssText = `
   height: 48px;
   cursor: pointer;
   z-index: 100000;
+  border-radius: 50%;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
 `;
 
 // popup
@@ -18,65 +20,76 @@ popup.style.cssText = `
   position: fixed;
   bottom: 60px;
   right: 10px;
-  width: auto;
-  height: auto;
-  max-width: 340px; 
-  background-color: #4CAF50; 
-  color: #333; 
-  border: 1px solid #ddd; 
-  border-radius: 8px; 
-  box-shadow: 0 4px 8px rgba(0,0,0,0.1); 
+  width: 300px;
+  background-color: #ffffff;
+  color: #333333;
+  border: none;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.1);
   z-index: 100001;
-  padding: 20px; 
-  box-sizing: border-box; 
+  padding: 20px;
+  box-sizing: border-box;
+  font-family: 'Arial', sans-serif;
+  line-height: 1.4;
 `;
 
 // content
 popup.innerHTML = `
-    <div id="popup-container" style="text-align: center;">
-    <label for="language-select" style="display: block; margin-bottom: 10px;">Select Your language:</label>
-    <select id="language-select" style="width: 100%; margin-bottom: 20px;">
-    <option value="English">English</option>
-    <option value="Chinese">Chinese</option>
-    <option value="Spanish">Spanish</option>
-    <option value="French">French</option>
-    <option value="Japanese">Japanese</option>
-    <!-- Add more language options here -->
-    </select>
-
-    <label for="reading-level-select" style="display: block; margin-bottom: 10px;">Select Your Reading Level:</label>
-    <select id="reading-level-select" style="width: 100%; margin-bottom: 20px;">
-    <option value="Beginner">Beginner: Age 5 - 10</option>
-    <option value="Intermediate">Intermediate: Age 11 - 15</option>
-    <option value="Advanced">Advanced: Age 16 - 20</option>
-    <!-- Add more reading level options here -->
-    </select>
+  <div id="popup-container" style="text-align: center;">
+    <div style="margin-bottom: 20px;">
+    <label for="language-select" style="display: block; margin-bottom: 10px;"><span style="font-weight: bold;">Select Your Language:</span></label>
+    <select id="language-select" style="width: 100%; padding: 8px; border-radius: 4px; border: 1px solid #ddd; box-shadow: inset 0 1px 2px rgba(0,0,0,0.1);">
+        <option value="English">English</option>
+        <option value="Chinese">Chinese</option>
+        <option value="Spanish">Spanish</option>
+        <option value="French">French</option>
+        <option value="Japanese">Japanese</option>
+      </select>
     </div>
+
+    <div style="margin-bottom: 20px;">
+    <label for="reading-level-select" style="display: block; margin-bottom: 10px;"><span style="font-weight: bold;">Select Your Reading Level:</span></label>
+    <select id="reading-level-select" style="width: 100%; padding: 8px; border-radius: 4px; border: 1px solid #ddd; box-shadow: inset 0 1px 2px rgba(0,0,0,0.1);">
+        <option value="Beginner">Beginner: Age 5 - 10</option>
+        <option value="Intermediate">Intermediate: Age 11 - 15</option>
+        <option value="Advanced">Advanced: Age 16 - 20</option>
+      </select>
+    </div>
+    
+    <button id="popup-submit" style="width: 100%; padding: 10px 20px; border-radius: 5px; border: none; background-color: #55935a; color: white; cursor: pointer; box-shadow: 0 2px 4px rgba(0,0,0,0.2); font-weight: bold;">Submit</button>
+  </div>
 `;
 
 document.body.appendChild(extensionIcon);
 document.body.appendChild(popup);
 
+// open when click, close after click again
 extensionIcon.addEventListener('click', function(event) {
-  popup.style.display = (popup.style.display === 'none' ? 'block' : 'none');
-  event.stopPropagation(); 
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('popup-close').addEventListener('click', function() {
-    popup.style.display = 'none';
-  });
-
-  window.addEventListener('click', function(event) {
-    if (!popup.contains(event.target) && !extensionIcon.contains(event.target)) {
+    if (popup.style.display === 'none') {
+      popup.style.display = 'block';
+    } else {
       popup.style.display = 'none';
     }
-  });
-
-  popup.addEventListener('click', function(event) {
     event.stopPropagation();
-  });
 });
 
+// close when click else where on the webpage
+window.addEventListener('click', function() {
+    if (popup.style.display === 'block') {
+        popup.style.display = 'none';
+    }
+});
 
+// not close when click the popup
+popup.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
 
+// get the result after submit
+document.getElementById('popup-submit').addEventListener('click', function() {
+    const language = document.getElementById('language-select').value;
+    const readingLevel = document.getElementById('reading-level-select').value;
+
+    // close the popup
+    popup.style.display = 'none';
+});
